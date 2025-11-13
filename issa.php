2d0346ee11da1841e1d0ba091a4208a95750dae7
issadakdouk <3isadakdok1234@gmail.com>
2025-11-13 12:48:22 +0200

    newfiles

---
+++ issa.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="issa.php"method="post">
+        <input type="text" name="username">
+        <input type="email" name="email">
+        <input type="password" name="password">
+        <input type="submit" name="submit"value="login">
+
+
+    </form>
+    
+</body>
+</html>
